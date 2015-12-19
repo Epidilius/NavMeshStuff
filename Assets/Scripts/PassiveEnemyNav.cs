78ebb841daaf20ce472315c89d1deaec77a2445e
Epidilius <joel.cright@gmail.com>
2015-12-19 15:18:34 -0500

    Added Lin of Sight
    Added a LoS check before the enemy starts chasing the player.

--- Assets/Scripts/PassiveEnemyNav.cs
+++ Assets/Scripts/PassiveEnemyNav.cs
@@ -5,6 +5,8 @@ public class PassiveEnemyNav : MonoBehaviour {
 
 	Transform target;
 	NavMeshAgent agent;
+	public float fieldOfView;	//TODO: Add to stats
+	public float rangeOfView;
 	bool isTimerRunning;
 	float timerTime;
 	float chaseTime;
@@ -13,6 +15,8 @@ public class PassiveEnemyNav : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		target = null;
+		fieldOfView = 90.0f;
+		rangeOfView = 1800.0f;
 		timerTime = 0.0f;
 		isTimerRunning = false;
 		chaseTime = 5.0f;
@@ -22,11 +26,29 @@ public class PassiveEnemyNav : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(target != null)
-			agent.SetDestination (target.position);
+		if (target != null) {
+			//TODO: Make this a little rougher. Use transform.position?
+			if (agent.transform == target.transform) {
+				//TODO: New random target in area. Start timer.
+			}
+
+			if(target.tag == "Player") {
+				agent = parent.GetComponentInChildren<NavMeshAgent> ();
+				//Vector3 enemyHeading = agent.transform.forward.normalized;
+				//Vector3 headingToPlayer = (target.transform.position - agent.transform.position).normalized;
+				//float dotProduct = Vector3.Dot(enemyHeading, headingToPlayer);
+
+				if(CanISeeTarget() == true) {
+					agent.SetDestination (target.position);
+				}
+			} else {
+				agent.SetDestination(target.position);
+			}
+		}
 
 		if(isTimerRunning == true) {
 			if (timerTime >= 0.0f) {
+				//TODO: Better subtraction, try to get once per second
 				timerTime--;
 				Debug.Log("Timer time: " + timerTime.ToString());
 			} else {
@@ -36,6 +58,37 @@ public class PassiveEnemyNav : MonoBehaviour {
 		}
 	}
 
+	bool CanISeeTarget() {
+		RaycastHit hit;
+		Vector3 rayDir = target.transform.position - agent.transform.position;
+
+		//If the target is close to the object, and in front, return true
+		float angle = Vector3.Angle (rayDir, agent.transform.forward);
+		float distance = Vector3.Distance (agent.transform.position, target.transform.position);
+		if ( angle < fieldOfView / 2.0f && distance < rangeOfView) {
+			Debug.Log("In front of enemy");
+			return true;
+		}
+
+		//Is the player within the enemies field of view
+		if ((Vector3.Angle (rayDir, agent.transform.forward)) < fieldOfView / 2.0f) {
+			Debug.Log ("Player within FoV");
+
+			if (Physics.Raycast (agent.transform.position, rayDir, out hit, rangeOfView)) {
+				if (hit.collider.gameObject == target) {
+					Debug.Log ("Raycast hit player");
+					return true;
+				} else {
+					Debug.Log ("Raycast did not hit player");
+					return false;
+				}
+			}
+		} else {
+			return false;
+		}
+
+		return false;
+	}
 	//Player enters Chase Radius
 	void OnTriggerEnter (Collider aTriggerer) 
 	{
---
+++ Assets/Scripts/StatsPassiveEnemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PassiveEnemyStats : MonoBehaviour {
+
+	public float perception;
+
+	// Use this for initialization
+	void Start () {
+		perception = 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
